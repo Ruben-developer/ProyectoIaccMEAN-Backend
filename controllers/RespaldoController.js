2c4b15cbb308d3956601591f82328128e25c4a63
Ruben-developer <rcontreras.dev@protonmail.com>
2021-08-09 17:18:40 -0400

    cambio en rutas de respaldo

--- controllers/RespaldoController.js
+++ controllers/RespaldoController.js
@@ -1,13 +1,13 @@
 function respaldar(){
     const execSync = require('child_process').execSync;    
-    const output = execSync('mongodump --db sistemadb --out /home/nbk/workspace/sistema-ventas-stack-mean-master/back/backups/backup-`date +"%d-%m-%Y_%H:%M:%S"`', { encoding: 'utf-8' });  
+    const output = execSync('mongodump --db sistemadb --out ./backups/backup-`date +"%d-%m-%Y_%H:%M:%S"`', { encoding: 'utf-8' });  
     console.log('Respaldar: '+ output);
     
 }
 
 function listar(req,res){
     const execSync = require('child_process').execSync;
-    const output = execSync('ls /home/nbk/workspace/sistema-ventas-stack-mean-master/back/backups/', { encoding: 'utf-8' });  
+    const output = execSync('ls ./backups/', { encoding: 'utf-8' });  
     var files_data = output.split('\n');
     files_data = cleanArray(files_data);
     console.log('comando:\n', output);
@@ -34,7 +34,7 @@ function restaurar(req, res){
     let backup = req.params['backup'];
 
     const execSync = require('child_process').execSync;    
-    const output = execSync('mongorestore --db sistemadb --drop /home/nbk/workspace/sistema-ventas-stack-mean-master/back/backups/'+backup+'/sistemadb', { encoding: 'utf-8' });  
+    const output = execSync('mongorestore --db sistemadb --drop ./backups/'+backup+'/sistemadb', { encoding: 'utf-8' });  
     console.log('Restaurado con exito: ' + output);
 
     if(output){
@@ -50,7 +50,7 @@ function eliminar(req, res){
     let backup = req.params['backup'];
     console.log('backup: '+backup);
     const execSync = require('child_process').execSync;
-    const output = execSync('sudo rm -fr /home/nbk/workspace/sistema-ventas-stack-mean-master/back/backups/'+backup,{ encoding: 'utf-8' });  
+    const output = execSync('sudo rm -fr ./backups/'+backup,{ encoding: 'utf-8' });  
     if(output){
         res.status(200).send({respaldo:output});
     }else{
--- routes/bitacora.js
+++ routes/bitacora.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bitacoraController = require('../controllers/BitacoraController');
 
-var app = express.Router();
+var api = express.Router();
 
 api.get('/api/seguridad/bitacora', bitacoraController.listar);
 api.post('/api/seguridad/bitacora/registrar', bitacoraController.registrar);
